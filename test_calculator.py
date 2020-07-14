a1f13f0336c7c67f6db8f5927aebee817d3f9fcc
Matheus P Moreira <matheus.paula.moreira@usp.br>
2020-07-14 11:23:29 -0300

    Test update - flake8

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,10 @@
 import calculator
 
-class TestCalculator:
-    
-    def test_addition(self):
-        assert 4 ==calculator.add(2,2)
 
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
 
     def test_substraction(self):
-        assert 2 == calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
